afc495ea6653286ca61fdb774869f514d25c8bf8
HongxuanG <1359774872@qq.com>
2022-06-05 13:27:47 +0800

    provide & inject

--- src/runtime-core/apiInject.ts
+++ src/runtime-core/apiInject.ts
@@ -8,29 +8,29 @@ export function provide<T>(key: string | number, value: T){
   const currentInstance: any = getCurrentInstance()
   if(currentInstance){
     let { provides } = currentInstance
-    // const parentProvides = currentInstance.parent?.provides
-    // if(provides === parentProvides){
-    //   // 把provide原型指向父组件的provide
-    //   provides = currentInstance.provides = Object.create(parentProvides)
-    // }
+    const parentProvides = currentInstance.parent?.provides
+    if(provides === parentProvides){
+      // 把provide原型指向父组件的provide
+      provides = currentInstance.provides = Object.create(parentProvides)
+    }
     provides[key] = value
   }
 }
-export function inject<T>(key: string, defaultValue?: unknown){
+export function inject<T>(key: string, defaultValue?: T){
   // 接收者
   // 在哪里拿value呢？在instance的parent上面获取到父组件的instance然后点出provide
   const currentInstance: any = getCurrentInstance()
   if(currentInstance){
     const parentProvides = currentInstance.parent.provides
 
-    // if (key in parentProvides){
+    if (key in parentProvides){
       return parentProvides[key]
-    // }else{  // 找不到注入的
-    //   // 如果默认值是函数，执行函数
-    //   if (isFunction(defaultValue)) {
-    //     return defaultValue()
-    //   }
-    //   return defaultValue
-    // }
+    }else{  // 找不到注入的
+      // 如果默认值是函数，执行函数
+      if (isFunction(defaultValue)) {
+        return defaultValue()
+      }
+      return defaultValue
+    }
   }
 }
